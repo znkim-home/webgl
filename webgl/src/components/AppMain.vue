56ecbd31a6ff593e30698c74df1b10868fd7b668
jinope <gbmin14@naver.com>
2022-07-27 15:58:34 +0900

    feat : change to colorful rectangle

--- webgl/src/components/AppMain.vue
+++ webgl/src/components/AppMain.vue
@@ -10,15 +10,22 @@
 
 const vertexShaderSource = `
   attribute vec4 aVertexPosition;
+  attribute vec4 aVertexColor;
+
   uniform mat4 uModelViewMatrix;
   uniform mat4 uProjectionMatrix;
+
+  varying lowp vec4 vColor;
   void main(void) {
     gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    vColor = aVertexColor;
   }
 `;
 const fragmentShaderSource = `
+  varying lowp vec4 vColor;
+
   void main(void) {
-    gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+    gl_FragColor = vColor;
   }
 `;
 export default {
@@ -30,11 +37,9 @@ export default {
       shaderProgram: null,
       fragmentShader: null,
       vertexShader: null,
-      squareVerticesBuffer: null,
-      squareVerticesBuffer2: null,
       mvMatrxi: null,
       perspectiveMatrix: null,
-      programInfo: null
+      programInfo: null,
     }
   },
   mounted() {
@@ -70,15 +75,17 @@ export default {
       if (!this.gl.getProgramParameter(this.shaderProgram, this.gl.LINK_STATUS)) {
         alert("Unable to initialize the shader program.");
       }
+
       this.programInfo = {
         program: this.shaderProgram,
         attribLocations: {
           vertexPosition: this.gl.getAttribLocation(this.shaderProgram, 'aVertexPosition'),
+          vertexColor: this.gl.getAttribLocation(this.shaderProgram, "aVertexColor")
         },
         uniformLocations: {
           projectionMatrix: this.gl.getUniformLocation(this.shaderProgram, 'uProjectionMatrix'),
           modelViewMatrix: this.gl.getUniformLocation(this.shaderProgram, 'uModelViewMatrix'),
-        },
+        }
       };
       this.gl.useProgram(this.shaderProgram);
     },
@@ -88,16 +95,32 @@ export default {
       this.setShaderProgram(this.vertexShader, this.fragmentShader);
     },
     initBuffers() {
+      let gl = this.gl;
+
       const positions = [
         1.0,  1.0,
         -1.0,  1.0,
         1.0, -1.0,
         -1.0, -1.0,
       ];
-      let gl = this.gl;
-      this.squareVerticesBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ARRAY_BUFFER, this.squareVerticesBuffer);
+      let squareVerticesBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+      const colors = [
+        1.0,  1.0,  1.0,  1.0,    // 흰색
+        1.0,  0.0,  0.0,  1.0,    // 빨간색
+        0.0,  1.0,  0.0,  1.0,    // 녹색
+        0.0,  0.0,  1.0,  1.0     // 파란색
+      ];
+      let squareVerticesColorBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesColorBuffer);
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+      this.programInfo.buffers = {
+        positionBuffer : squareVerticesBuffer,
+        colorBuffer : squareVerticesColorBuffer
+      }
     },
     getShader(type) {
       let gl = this.gl;
@@ -135,14 +158,17 @@ export default {
       const zFar = 100.0;
       const projectionMatrix = self.glMatrix.mat4.create();
       self.glMatrix.mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+
       const modelViewMatrix = self.glMatrix.mat4.create();
       self.glMatrix.mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
+
       {
         const numComponents = 2;
         const type = this.gl.FLOAT;
         const normalize = false;
         const stride = 0;
         const offset = 0;
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.programInfo.buffers.positionBuffer);
         this.gl.vertexAttribPointer(
             this.programInfo.attribLocations.vertexPosition,
             numComponents,
@@ -150,9 +176,26 @@ export default {
             normalize,
             stride,
             offset);
-        this.gl.enableVertexAttribArray(
-            this.programInfo.attribLocations.vertexPosition);
+        this.gl.enableVertexAttribArray(this.programInfo.attribLocations.vertexPosition);
       }
+
+      {
+        const numComponents = 4;
+        const type = this.gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.programInfo.buffers.colorBuffer);
+        this.gl.vertexAttribPointer(
+            this.programInfo.attribLocations.vertexColor,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        this.gl.enableVertexAttribArray(this.programInfo.attribLocations.vertexColor);
+      }
+
       this.gl.uniformMatrix4fv(
         this.programInfo.uniformLocations.projectionMatrix,
         false,
@@ -161,7 +204,12 @@ export default {
         this.programInfo.uniformLocations.modelViewMatrix,
         false,
         modelViewMatrix);
-      this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
+        
+      {
+        const offset = 0;
+        const vertexCount = 4;
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP, offset, vertexCount);
+      }
     }
   }
 }
